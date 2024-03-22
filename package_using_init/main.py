dd1e86cf8ffc0985707f8ec9e9d3cef8e701e8fe
bithealthdenta <marcellus.denta@bithealth.co.id>
2024-03-23 00:19:36 +0700

    modified of using __init__.py

--- package_using_init/main.py
+++ package_using_init/main.py
@@ -1,5 +1,5 @@
 import sains
-hasil_tambah=sains.matematika.basic.kali(1,2,3,4,5)
+hasil_tambah=sains.matematika.kali(1,2,3,4,5)
 hasil_gaya=sains.fisika.gaya(2,5)
 print(hasil_tambah)
 print(hasil_gaya)
